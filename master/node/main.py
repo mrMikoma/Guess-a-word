7b0e0883a50a59318054bb52cd7eb9ef93fdc952
jerew <jere.wahlsten@gmail.com>
2024-04-18 16:34:10 +0300

    Added serve() invoke Added TODO started work on master

--- master/node/main.py
+++ master/node/main.py
@@ -1,6 +1,64 @@
+from concurrent import futures
+import time
+import grpc
+import os
+import master_pb2_grpc
+import master_pb2
+import fastapi
+from dotenv import load_dotenv
 
-def main():
-    print("Hello from the master node module!")
-    return 0
+# Global variables
+MAX_WORKERS = 10
+APP = fastapi.FastAPI()
+PORT = 50051
+WORKER_LOBBIES = {} # dictionary {worker:lobby_count} to track how many lobbies each worker has
 
-main()
+load_dotenv()
+
+def AddAddNewLobbyToDB():
+    # TODO
+    print("added a new lobby!")
+    return
+
+def JoinExistingLobby():
+    # TODO
+    print("joined a new lobby")
+    return
+class MasterServiceServicer(master_pb2_grpc.MasterServiceServicer): 
+    
+    def SendLobbyInfo(self, request, context):
+        ip=-1
+        lobby_id=-1
+        if (request.lobby_choice == 1):
+            ip, lobby_id = self.JoinExistingLobby()
+        elif(request.lobby_choise == 2):
+            ip, lobby_id = self.AddNewLobbyToDB()
+        else:
+            # wrong request, return default values 
+            pass
+        return master_pb2.NewLobbyInfo(ip=ip, lobby_id=lobby_id)
+
+def initialize():
+    # TODO
+    # clear up
+    return
+    
+def serve():
+    # Initialize data structures
+    initialize()
+    
+    # Initialize the server
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=MAX_WORKERS))
+    master_pb2_grpc.add_MasterServiceServicer_to_server(MasterServiceServicer(), server)
+    server.add_insecure_port(f'[::]:{PORT}')
+    server.start()
+    try:
+        while True:
+            print(f"Server is running on port {PORT}...")
+            time.sleep(86400)  # One day
+    except KeyboardInterrupt:
+        server.stop(0)
+
+
+if __name__ == "__main__":
+    serve()
