e1897d2a999d2840f19d0c85101f34143df9e0ac
jerew <jere.wahlsten@gmail.com>
2024-04-23 02:32:08 +0300

    - made docker-compose for master (to prevent it from creating millions of images) - fixed master

--- master/node/main.py
+++ master/node/main.py
@@ -21,50 +21,59 @@ load_dotenv()
 
 class MasterServiceServicer(master_pb2_grpc.MasterServiceServicer, sys_master_pb2_grpc.SysMasterServiceServicer): 
     
-    def UpdateWorkers():
+    def UpdateWorkers(self):
         try:
-            worker_list = list(requests.get(DB_ADDRESS+"/workers/").content)
+            worker_list = list(requests.get(DB_ADDRESS+"/workers/").json())
             for worker in worker_list:
-                if worker in WORKER_LOBBIES:
+                worker_ip = worker["ip_address"]
+                if worker_ip in WORKER_LOBBIES:
                     continue
                 else:
-                    WORKER_LOBBIES[worker]=0
+                    WORKER_LOBBIES[worker_ip]=0
+                    print(f"added new worker '{worker_ip}' to the list!")
         except Exception as e:
             print("Error Adding new workers:",e)
         finally:
             return
-        return
     def CreateNewLobby(self, request, context):
-        ip, lobby_id = -1, -1
+        ip = ""
+        lobby_id = -1
         try: 
             lobby_id = requests.post(url=DB_ADDRESS+"/lobbies/").json()["lobby_id"]
-            
+            USER_ID = request.user_id
             # Update workers before using them
             self.UpdateWorkers()
             # Find the worker with the smallest lobby_count
             ip = min(WORKER_LOBBIES, key=lambda x: WORKER_LOBBIES[x])
             with grpc.insecure_channel(f"{ip}:50052") as channel:
-                workerStub = sys_worker_pb2_grpc.WorkerServiceStub(channel)
-                response = workerStub.NewLobby(sys_master_pb2.LobbyParams(lobby_id=lobby_id, user_id=request.user_id))
+                workerStub = sys_worker_pb2_grpc.SysWorkerServiceStub(channel)
+                print("check 1") # ALEMPI EI TOIMI 
+                print(f"lobby id: {lobby_id} #### user_id: {request.user_id}") #DEBUG
+                request = sys_worker_pb2.LobbyParams(lobby_id=lobby_id, user_id=USER_ID,)
+                print("ip:",ip) 
+                response = workerStub.NewLobby(request)
+                print("check 13")
                 if response.status == "OK":
-                    request = requests.put(url=DB_ADDRESS+"/lobbies/"+lobby_id, data={"lobby_id": lobby_id, "ip_address": ip, "status": "available"})
-                    return master_pb2.LobbyInfo(ip=ip, lobby_id=lobby_id)
+                    request = requests.put(url=DB_ADDRESS+"/lobbies/"+str(lobby_id), params={"lobby_id": lobby_id, "ip_address": ip, "status": "available"})
+                    print("check 2")
+                    return master_pb2.LobbyInfo(ip=str(ip), lobby_id=int(lobby_id))
                 else:
                     print("Error with worker:", response.status, response.desc)
                     return master_pb2.LobbyInfo(ip=-1, lobby_id=-1)
         except Exception as e:
             print("Error: ", e)
-            return master_pb2.LobbyInfo(ip=-1, lobby_id=-1)
+            return master_pb2.LobbyInfo(ip=str(ip), lobby_id=int(lobby_id))
     
     def JoinLobby(self, request, context):
         try:
-            ip, lobby_id = -1
+            ip = ""
+            lobby_id = request.lobby_id
             lobby = requests.get(url=DB_ADDRESS+"/lobbies/"+request.lobby_id).json()
             ip = lobby["ip_address"]
-            return master_pb2.LobbyInfo(ip=ip, lobby_id=request.lobby_id)
+            return master_pb2.LobbyInfo(ip=ip, lobby_id=lobby_id)
         except Exception as e:
             print("Error: ", e)
-            return master_pb2.LobbyInfo(ip=-1, lobby_id=-1)
+            return master_pb2.LobbyInfo(ip=ip, lobby_id=lobby_id)
     
     def UpdateLobby(self, request, context):
         status, desc = ""
@@ -83,14 +92,7 @@ class MasterServiceServicer(master_pb2_grpc.MasterServiceServicer, sys_master_pb
 
 def initialize():
     # add every worker to dict and set their lobby count to 0
-    try:
-        worker_list = list(requests.get(DB_ADDRESS+"/workers/").content)
-        for worker in worker_list:
-            WORKER_LOBBIES[worker]=0
-    except Exception as e:
-        print("Error initializing workers:",e)
-    finally:
-        return
+    MasterServiceServicer.UpdateWorkers(self=MasterServiceServicer)
     
 def serve():
     initialize()
--- master/node/sys_worker_pb2.py
+++ master/node/sys_worker_pb2.py
@@ -14,7 +14,7 @@ _sym_db = _symbol_database.Default()
 
 
 
-DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x10sys-worker.proto\x12\x06worker\"0\n\x0bLobbyParams\x12\x10\n\x08lobby_id\x18\x01 \x01(\t\x12\x0f\n\x07user_id\x18\x02 \x01(\t\",\n\x0cMasterStatus\x12\x0e\n\x06status\x18\x01 \x01(\t\x12\x0c\n\x04\x64\x65sc\x18\x02 \x01(\t2\x8b\x01\n\x10SysWorkerService\x12\x37\n\x08NewLobby\x12\x13.worker.LobbyParams\x1a\x14.worker.MasterStatus\"\x00\x12>\n\x0fMasterJoinLobby\x12\x13.worker.LobbyParams\x1a\x14.worker.MasterStatus\"\x00\x62\x06proto3')
+DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x10sys-worker.proto\x12\x06worker\"0\n\x0bLobbyParams\x12\x10\n\x08lobby_id\x18\x01 \x01(\x03\x12\x0f\n\x07user_id\x18\x02 \x01(\t\",\n\x0cMasterStatus\x12\x0e\n\x06status\x18\x01 \x01(\t\x12\x0c\n\x04\x64\x65sc\x18\x02 \x01(\t2\x8b\x01\n\x10SysWorkerService\x12\x37\n\x08NewLobby\x12\x13.worker.LobbyParams\x1a\x14.worker.MasterStatus\"\x00\x12>\n\x0fMasterJoinLobby\x12\x13.worker.LobbyParams\x1a\x14.worker.MasterStatus\"\x00\x62\x06proto3')
 
 _globals = globals()
 _builder.BuildMessageAndEnumDescriptors(DESCRIPTOR, _globals)
